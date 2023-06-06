d8622cf8e8bbef51ee62f70e04b563f5301cd87b
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-06 23:28:39 +0800

    配置开发服务器，开发环境允许跨域

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,38 @@ import vue from '@vitejs/plugin-vue'
 // element-plus:按需导入的自动导入插件
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver, ElementUiResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// vite 按需导入组件库样式
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    open: '/login',
+    cors: true //开发服务器下允许跨域
+  },
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver(), ElementUiResolver()]
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver(), ElementUiResolver()],
+      resolvers: [ElementPlusResolver()],
       dts: true
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name: string) => {
+            return `element-plus/theme-chalk/${name}.css`
+          }
+        }
+      ]
     })
   ],
   resolve: {
