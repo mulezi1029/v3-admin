a4919c61c43f4c239d104f4bca7b4c0a9773e55d
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-06 17:14:15 +0800

    解决element-plus按需导入没有类型的声明以及文件中导入显示错误

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,17 @@ import vue from '@vitejs/plugin-vue'
 // element-plus:按需导入的自动导入插件
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver, ElementUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
-      dts: './auto-imports.d.ts'
+      resolvers: [ElementPlusResolver(), ElementUiResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver(), ElementUiResolver()],
       dts: true
     })
   ],
