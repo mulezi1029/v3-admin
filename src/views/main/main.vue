b241b12bb9d895e94492e0d4e8c98e3077d6783e
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-07 11:17:26 +0800

    首页退出登陆逻辑

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
+    <el-button type="warning" @click="handleLogOut">退出登陆</el-button>
     <el-scrollbar height="400px">
       <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
     </el-scrollbar>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+// 退出登陆逻辑
+const handleLogOut = () => {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
+</script>
 
 <style scoped></style>
