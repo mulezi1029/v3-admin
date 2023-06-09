98651a10daa67231ca814cf630cf18c932d94485
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-09 11:01:44 +0800

    首页基本布局搭建

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <el-button type="warning" @click="handleLogOut">退出登陆</el-button>
-    <el-scrollbar height="400px">
-      <p v-for="item in 20" :key="item" class="scrollbar-demo-item">{{ item }}</p>
-    </el-scrollbar>
+  <div class="main">
+    <!-- <el-button type="warning" @click="handleLogOut">退出登陆</el-button> -->
+    <el-container class="main-content">
+      <!-- 侧边栏 -->
+      <el-aside width="200px" class="el-aside">Aside</el-aside>
+      <!-- 内容 -->
+      <el-container>
+        <!-- 头部 -->
+        <el-header class="el-header">Header</el-header>
+        <!-- 主体 -->
+        <el-main class="el-main">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -21,4 +29,20 @@ const handleLogOut = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.main {
+  height: 100%;
+  .main-content {
+    height: 100%;
+    .el-aside {
+      background-color: aqua;
+    }
+    .el-header {
+      background-color: aquamarine;
+    }
+    .el-main {
+      background-color: bisque;
+    }
+  }
+}
+</style>
