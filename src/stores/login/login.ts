755bff39e81233336a4b2b4382ca5a38c6852e88
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-12 15:11:03 +0800

    本地缓存用户登陆数据与菜单数据

--- src/stores/login/login.ts
+++ src/stores/login/login.ts
@@ -14,8 +14,8 @@ interface ILoginState {
 const useLoginStore = defineStore('login', {
   state: (): ILoginState => ({
     token: localStorage.getItem(LOGIN_TOKEN) ?? '',
-    userInfo: {},
-    userMenus: []
+    userInfo: localCache.getCache('userInfo') ?? {},
+    userMenus: localCache.getCache('userMenus') ?? []
   }),
 
   actions: {
@@ -23,21 +23,21 @@ const useLoginStore = defineStore('login', {
       // 1.账号登陆，获取登陆的 token 信息
       const loginData = await loginAccount(account)
 
-      this.token = loginData.data.token
       const id = loginData.data.id
+      this.token = loginData.data.token
+      localCache.setCatche(LOGIN_TOKEN, this.token) // 本地缓存token
 
-      // 2.本地缓存token
-      localCache.setCatche(LOGIN_TOKEN, this.token)
-
-      // 3.获取用户对应的详细信息：角色信息（role）
+      // 2.获取用户对应的详细信息：角色信息（role）
       const { data: userInfo } = await getUserInfoById(id)
       this.userInfo = userInfo
+      localCache.setCatche('userInfo', userInfo) //本地缓存用户登陆信息
 
-      // 4.根据角色获取对应的权限菜单数组
+      // 3.根据角色获取对应的权限菜单数组
       const userMenuInfo = await getUserMenusByRole(this.userInfo.role.id)
       this.userMenus = userMenuInfo.data
+      localCache.setCatche('userMenus', this.userMenus) //本地缓存用户菜单信息
 
-      // 3.登陆成功进行路由跳转
+      // 4.登陆成功进行路由跳转
       router.push('/main')
     }
   }
