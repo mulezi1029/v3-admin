90a8bf86936940b1c8ea3f86db1efddab0f8237c
edwardchen_i <edwardchen_i@didiglobal.com>
2023-06-12 15:13:51 +0800

    侧边栏菜单结构搭建与数据渲染

---
+++ src/components/main-menu/mian-menu.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="main-menu">
+    <!-- logo -->
+    <div class="logo">
+      <img src="@/assets/img/logo.svg" alt="" />
+      <h2 class="title">后台管理系统</h2>
+    </div>
+
+    <!-- 菜单 -->
+    <div class="menu">
+      <el-menu
+        :default-active="userMenus[0].children[0].id"
+        :collapse="false"
+        background-color="#001529"
+        text-color="#b7bdc3"
+        active-text-color="#fff"
+      >
+        <!-- 2. 遍历菜单数组，渲染出菜单 -->
+        <template v-for="item in userMenus" :key="item.id">
+          <el-sub-menu :index="item.id">
+            <!-- 标题 -->
+            <template #title>
+              <el-icon>
+                <!-- 图标 -->
+                <component :is="item.icon.split('el-icon')[1]" />
+              </el-icon>
+              <span>{{ item.name }}</span>
+            </template>
+
+            <!-- 子菜单项 -->
+            <template v-for="menu in item.children" :key="menu.id">
+              <el-menu-item :index="menu.id">{{ menu.name }}</el-menu-item>
+            </template>
+          </el-sub-menu>
+        </template>
+
+        <!-- 1. 静态菜单结构 -->
+        <!-- <el-sub-menu index="1">
+          <template #title>
+            <el-icon><Monitor /></el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item index="1-1">核心技术</el-menu-item>
+          <el-menu-item index="1-2">商品统计 </el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon><Setting /></el-icon>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item index="2-1">用户管理</el-menu-item>
+          <el-menu-item index="2-2">部门管理 </el-menu-item>
+          <el-menu-item index="2-3">菜单管理 </el-menu-item>
+          <el-menu-item index="2-4">角色管理 </el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="3">
+          <template #title>
+            <el-icon><ShoppingBag /></el-icon>
+            <span>商品中心</span>
+          </template>
+          <el-menu-item index="3-1">商品类别</el-menu-item>
+          <el-menu-item index="3-2">商品信息 </el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon><ChatLineRound /></el-icon>
+            <span>随便聊聊</span>
+          </template>
+          <el-menu-item index="4-1">你的故事</el-menu-item>
+          <el-menu-item index="4-2">故事列表 </el-menu-item>
+        </el-sub-menu> -->
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useLoginStore from '@/stores/login/login'
+
+const loginStore = useLoginStore()
+const userMenus = loginStore.userMenus
+</script>
+
+<style scoped lang="less">
+.main-menu {
+  height: 100%;
+  background-color: #001529;
+  .logo {
+    display: flex;
+    height: 24px;
+    padding: 15px 10px 8px 10px;
+
+    img {
+      height: 100%;
+      margin: 0 10px;
+    }
+    .title {
+      color: white;
+      font-size: 24px;
+      font-weight: normal;
+    }
+  }
+
+  .el-menu {
+    border-right: none;
+    user-select: none; //阻止双击选中文本显示高亮
+
+    .el-sub-menu {
+      .el-menu-item {
+        padding-left: 50px;
+        background-color: #0c2135;
+      }
+      .el-menu-item:hover {
+        color: #fff;
+      }
+      .el-menu-item.is-active {
+        background-color: #0a60bd;
+      }
+    }
+  }
+}
+</style>
